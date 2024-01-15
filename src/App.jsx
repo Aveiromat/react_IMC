c72a2fdcf49ee9b99ecb608a9cadea2f3cc5b6da
Aveiro <matheusaveirofagundes@gmail.com>
2024-01-15 15:12:59 -0300

    commit inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,36 @@
+import { useState } from "react";
+
 import Perfil from "./components/Perfil";
 import Formulario from "./components/Formulario";
+import ReposList from "./components/ReposList";
 
 function App() {
-  const nome = "Matheus";
-
-  const pessoa = {
-    nome: "Maria Fernanda"
-  }
-
-  let estaDeDia = true;
+  const [formularioEstaVisivel, setFormularioEstaVisivel] = useState(true);
+  const [nomeUsuario, setNomeUsuario] = useState('');
 
   return (
     <>
-      <Perfil nome="matheus" endereco="https://github.com/aveiromat.png" />
-      <Formulario />
+      <input type="text" onBlur={(e) => setNomeUsuario(e.target.value)} />
+
+      {nomeUsuario.length > 4 && (
+        <>
+          <Perfil nomeUsuario={nomeUsuario} />
+          <ReposList nomeUsuario={nomeUsuario} />
+        </>
+      )}
+
+      {/* {formularioEstaVisivel && ( //mount /  onmount
+        <Formulario />
+      )}
+
+      <button onClick={() => setFormularioEstaVisivel(!formularioEstaVisivel)} type="button">toggle form</button> */}
     </>
   )
 }
 
 export default App
 
-  /* ESTUDOS
+/* ESTUDOS
 <h1>Olá, {pessoa.nome}</h1>
 <h2>Subtitulo</h2>
 {estaDeDia === true ? 'Bom dia' : 'Boa noite'}
--- src/components/Formulario/index.jsx
+++ src/components/Formulario/index.jsx
@@ -1,17 +1,65 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 const Formulario = () => {
-    let [materiaA, setMateriaA] = useState(0);
-    let [materiaB, setMateriaB] = useState(0);
-    let [materiaC, setMateriaC] = useState(0);
+    const [materiaA, setMateriaA] = useState(0);
+    const [materiaB, setMateriaB] = useState(0);
+    const [materiaC, setMateriaC] = useState(0);
+    const [nome, setNome] = useState('');
 
+    useEffect(() => {
+        console.log ("o componente iniciou");
+
+        return() => {
+            console.log("o componente finalizou")
+        }
+    }, [])
+
+    useEffect(() => {
+        console.log ("o estado nome mudou")
+    }, [nome])
+
+    useEffect(() => {
+        console.log ("materia A mudou para:" + materiaA)
+    }, [materiaA, materiaB, materiaC])
+
+    const alteraNome = (evento) => {
+    // console.log(evento.target.value)
+    // setNome(evento.target.value);
+    setNome(estadoAnterior => {
+        // console.log(estadoAnterior);
+
+        return evento.target.value;
+    })
+    }
+
+    const renderizaResultado = () => {
+        const soma = materiaA + materiaB + materiaC;
+        const media = soma / 3;
+
+        if (media >= 7) {
+            return (
+                <p>Olá {nome}, foi aprovado</p>
+            )
+        } else {
+            return (
+                <p>Olá {nome}, não foi aprovado</p>
+            )
+        }
+    }
 
     return (
         <form>
-            <input type="number" placeholder="Nota Matéria A" onChange={evento => console.log(evento.target.value)} />
-            <input type="number" placeholder="Nota Matéria B" />
-            <input type="number" placeholder="Nota Matéria C" />
-            <p>O aluno foi aprovado</p>
+            <ul>
+                {[1,2,3,4,5].map(item => (
+                    <li key={item} >{item}</li>
+                ))}
+            </ul>
+
+            <input type="text" placeholder="Seu nome" onChange={alteraNome} />
+            <input type="number" placeholder="Nota Matéria A" onChange={({ target }) => setMateriaA(parseInt(target.value))} />
+            <input type="number" placeholder="Nota Matéria B" onChange={evento => setMateriaB(parseInt(evento.target.value))}/>
+            <input type="number" placeholder="Nota Matéria C" onChange={evento => setMateriaC(parseInt(evento.target.value))}/>
+            {renderizaResultado()}
         </form>
     )
 }
